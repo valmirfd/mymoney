ee2ab60ccc861a7af55bd5ed428bc14608ea084e
Valmir Dornas <fernandocursos2021@gmail.com>
2025-08-20 10:19:15 -0300

    Header personalizado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,16 +3,16 @@ import React, { useContext } from "react";
 import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
 
+import Header from '../../components/Header';
+
+import { Background } from './styles';
+
 
 export default function Home() {
-    const { signOut } = useContext(AuthContext);
+
     return (
-        <View>
-            <Text>TELA HOME</Text>
-            <Button
-                title="Sair da conta"
-                onPress={() => signOut()}
-            />
-        </View>
+        <Background>
+            <Header title="Minhas movimentações" />
+        </Background>
     );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+
+
+export const Background = styled.SafeAreaView`
+    flex: 1;
+    background-color: #76E6DA;
+`;
