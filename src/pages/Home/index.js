b8099c50b094f311b3bd0614d7d937dc276e453f
Valmir Dornas <fernandocursos2021@gmail.com>
2025-08-20 23:16:05 -0300

    Listando movimentos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
 
 import Header from '../../components/Header';
-
 import { Background } from './styles';
+import api from "../../services/api";
+import { format } from "date-fns";
 
 
 export default function Home() {
+    const [listBalance, setListBalance] = useState([]);
+    const [dateMovements, setDateMovements] = useState(new Date());
+
+
+    useEffect(() => {
+        let isActive = true;
+
+        async function getMovements() {
+            let dateFormated = format(dateMovements, 'dd/MM/yyyy');
+
+            const balance = await api.get('/balance', {
+                params: {
+                    date: dateFormated
+                }
+            })
+
+            if (isActive) {
+                setListBalance(balance.data);
+            }
+        }
+
+        getMovements();
+
+        return () => isActive = false;
+
+    }, []);
+
 
     return (
         <Background>
